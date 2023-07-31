13e2bd26ec3ca167588ced6b552466a6477dad00
DanRoscigno <dan@roscigno.com>
2023-07-31 16:42:37 -0400

    add component

---
+++ components/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact =() => {
+  return (
+    <div>
+      <h2>Contact</h2>
+      You can contact us via{' '}
+      <a href="https://www.example.com">Example</a>
+    </div>
+  )
+}
+
+export default Contact
