7b61bed2f46231a3f69e12c1a17a887317afc9ab
DanRoscigno <dan@roscigno.com>
2023-09-27 13:43:11 -0400

    use vercel/SWR as example

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,10 +1,10 @@
 export default {
-  logo: <span>My Nextra Documentation</span>,
+  logo: <span>StarRocks Documentation</span>,
   project: {
-    link: 'https://github.com/shuding/nextra'
+    link: 'https://github.com/StarRocks/starrocks'
   },
   i18n: [
-  { locale: 'en', text: 'English' },
-  { locale: 'zh', text: '中文' }
+  { locale: 'en-US', text: 'English' },
+  { locale: 'zh-CN', text: '中文' }
   ]
 }
