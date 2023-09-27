51a324ccfc9f6c021ce733657174d5537be4defa
DanRoscigno <dan@roscigno.com>
2023-09-27 13:02:53 -0400

    WIP

--- theme.config.jsx
+++ theme.config.jsx
@@ -2,6 +2,9 @@ export default {
   logo: <span>My Nextra Documentation</span>,
   project: {
     link: 'https://github.com/shuding/nextra'
-  }
-  // ...
+  },
+  i18n: [
+  { locale: 'en', text: 'English' },
+  { locale: 'zh', text: '中文' }
+  ]
 }
